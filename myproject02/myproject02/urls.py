2cfc25ac2cb1118404cd7cc8782e680ca13ed9d1
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-08-29 13:35:50 +0900

    updated

--- myproject02/myproject02/urls.py
+++ myproject02/myproject02/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myproject03 project.
+URL configuration for myproject02 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -15,12 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from . import views
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.homepage),
-    path('about/', views.about),
-    path('posts/', include('posts.urls'))
 ]
