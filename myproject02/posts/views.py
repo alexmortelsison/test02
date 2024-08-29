ac90b924d2dba0666cfffc455a1db39dc671969f
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-08-29 14:04:04 +0900

    updated

--- myproject02/posts/views.py
+++ myproject02/posts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def posts_list(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/posts_list.html', {'posts': posts})
